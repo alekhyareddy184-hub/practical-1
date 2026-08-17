11d623037f38ff87a27ad885a87cc91c55d23614
alekhyareddy184-hub <chintaalekhyareddy.127766@marwadiuniversity.ac.in>
2026-08-17 22:02:18 +0530

    Bubble short.cpp

---
+++ Bubble short.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[] = {5, 3, 8, 4, 2};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
